7bcd065eed73ab7f67d59e072090e106f4af93f5
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2021-12-15 17:14:58 +0300

    Добавьте файлы проекта.

---
+++ privedenue tipov/Source.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+/*ѕривести, отличные от учебника, свои примеры :
+1) ѕреобразование €вное и не€вное в стиле C
+2) static_cast
+3) dynamic_cast
+4) const_cast
+5) „то использовать дл€ преобразовани€ родительского класса в дочерний ?
+ј дочернего в родительский ? «ачем ?*/
+void preobrazovanue_C(int one, double two) {
+	one = (int)two;
+	two = (double)one;   // €вное преобразование
+
+	two = one;
+	one = two; // не€вное преобразование
+}
+
+//double static__cast(int one, int two) {
+	//double result = static_cast<double> (one) / two;
+	//return result;
+
+//}
+int  const__cast(const int* as) {
+	int* sa;
+	sa = const_cast<int*>(as);  // больше не константа!
+	*sa = *as * *as;
+	return *sa;
+
+}
+
+
+
+
+class Base {
+public:
+	virtual void print() {
+		cout << "Base" << endl;
+	}
+
+};
+class A : public Base {
+public:
+	virtual void print() {
+		cout << "A" << endl;
+	}
+	string prob(string s) {
+		return s;
+	}
+
+};
+class C : public Base {
+public:
+	virtual void print() {
+		cout << "C" << endl;
+	}
+	string proa(string s) {
+		return s;
+	}
+};
+
+void wver(Base* one) {
+	A* two = dynamic_cast<A*>(one);
+	C* thee = dynamic_cast<C*>(one);
+	if (two) {
+		cout << two->prob("dfs") << endl;
+	}
+	else if (thee) {
+		cout << thee->proa("3") << endl;
+	}
+
+}
+
+int main() {
+	C thee;
+	wver(&thee);
+
+	Base* one, one_1;
+	A* two, two_2;
+
+	one = &two_2; //указывает на обьект производного класса
+	two = dynamic_cast<A*> (one);
+
+	int x = 10;
+	cout << const__cast(&x) << endl;
+	//cout << static__cast << endl;
+
+
+}
